4bd5cf0b44d1d8fd19c33d54b7893c2c41354277
nickcallaghan <nicholas.callaghan@gmail.com>
2019-09-17 00:51:48 +0100

    Updates

---
+++ precache-manifest.1db1012744e46516125b5184445e01d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea9203768dcb515dc33b5f61bee56e9",
+    "url": "/NickCallaghan/react-todo-app/index.html"
+  },
+  {
+    "revision": "36b3188c54a0f949c959",
+    "url": "/NickCallaghan/react-todo-app/static/css/main.97b27b26.chunk.css"
+  },
+  {
+    "revision": "4077a028c78f05e231b2",
+    "url": "/NickCallaghan/react-todo-app/static/js/2.9c499d9c.chunk.js"
+  },
+  {
+    "revision": "36b3188c54a0f949c959",
+    "url": "/NickCallaghan/react-todo-app/static/js/main.f70ed1c9.chunk.js"
+  },
+  {
+    "revision": "98a2d43a5cfccd3c682f",
+    "url": "/NickCallaghan/react-todo-app/static/js/runtime~main.d7159dc2.js"
+  }
+]);
