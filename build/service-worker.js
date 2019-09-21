87f0edfbc229e05ca7fdad8aa4014b7ffc14cde8
Nick Callaghan <Nick.Callaghan@zonal.co.uk>
2019-09-21 08:54:03 +0100

    adjusting variables for build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NickCallaghan/react-todo-app/precache-manifest.7597f45c678781098f90538379ad8cde.js"
+  "/react-todo-app/precache-manifest.88acdc896e95bac4d3869af75ec6b8dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NickCallaghan/react-todo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
