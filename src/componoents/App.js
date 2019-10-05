d521d42adeaf61922ef0e0a5222c143e31fe7aee
NickCallaghan <nicholas.callaghan@gmail.com>
2019-10-05 14:45:25 +0100

    fxing delete

--- src/componoents/App.js
+++ src/componoents/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import AddToDo from './AddToDo';
-import ToDoList from './ToDoList';
-import Stats from './Stats';
-import FilterBar from './FilterBar';
+import Main from '../pages/Main';
 
 class App extends React.Component {
-	state = {
-		todos: {
-			1: {
-				index: 1,
-				key: 1,
-				task: 'Feed the cat',
-				complete: true,
+	constructor(props) {
+		super(props);
+		this.state = {
+			todos: {
+				1: {
+					index: 1,
+					key: 1,
+					task: 'Feed the cat',
+					complete: true,
+				},
+				2: {
+					index: 2,
+					key: 2,
+					task: 'Walk the cat',
+					complete: false,
+				},
+				3: {
+					index: 3,
+					key: 3,
+					task: 'Play with the cat',
+					complete: false,
+				},
 			},
-			2: {
-				index: 2,
-				key: 2,
-				task: 'Walk the cat',
-				complete: false,
-			},
-			3: {
-				index: 3,
-				key: 3,
-				task: 'Play with the cat',
-				complete: false,
-			},
-		},
-		filter: 'all',
-	};
+			filter: 'all',
+		};
+	}
 
 	updateToDo = (key, updateToDo) => {
 		let todos = { ...this.state.todos };
@@ -51,18 +51,13 @@ class App extends React.Component {
 			<div className="todo-container">
 				<h1 className="main-heading">Happy Cat To Do List</h1>
 				<p className="sub-heading">Happy cats mean completed todos</p>
-
-				<AddToDo updateToDo={this.updateToDo} />
-
-				<FilterBar updateFilter={this.updateFilter} />
-
-				<ToDoList
+				<Main
 					filter={this.state.filter}
 					todos={this.state.todos}
 					updateToDo={this.updateToDo}
 					deleteToDo={this.deleteToDo}
+					updateFilter={this.updateFilter}
 				/>
-				<Stats todos={this.state.todos} />
 			</div>
 		);
 	}
